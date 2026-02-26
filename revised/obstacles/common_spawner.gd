8faf820b322e8fc3d863d4ad015956596799d04b
G4l3S <gabs.sumergido@gmail.com>
2026-02-27 03:46:02 +0800

    refactor: optimized code for better readability
    - incorporated scroll speed as default values for ground projectiles
    - adjusted projectiles spawned based on bird's level

--- revised/obstacles/common_spawner.gd
+++ revised/obstacles/common_spawner.gd
@@ -6,30 +6,32 @@ signal bird_hit
 @export var projectiles : Array[PackedScene]
 @export var max_spawn_interval := 3.0
 
-@onready var right: Marker2D = $Right
-@onready var ground: Marker2D = $Ground
-@onready var left: Marker2D = $Left
+@onready var right: float = $Right.global_position.x
+@onready var ground: float = $Ground.global_position.y
+@onready var left: float = $Left.global_position.x
+@onready var sky: float = $Sky.global_position.y
 
 @onready var spawn_timer: Timer = $SpawnTimer
-var bird = null
-
+var bird : Bird = null
+var speed : float
 var bird_level := 0
 
 func _ready() -> void:
 	spawn_timer.timeout.connect(spawn_projectiles)
 
-func parse(birdie : Node2D) -> void:
+func parse(birdie : Node2D, scroll : float) -> void:
 	bird = birdie
+	speed = scroll
 
 func start() -> void:
 	spawn_timer.start()
 
 func spawn_projectiles() -> void:
 	var queued : Array[Projectile]
-	for i in range(randi_range(1, 2 + bird_level)):
+	for i in range(randi_range(1, 1 + bird.level)):
 		var p = projectiles[
 			randi_range(0, projectiles.size()-1)].instantiate() as Projectile
-		p.setup_projectile(bird, left, right, ground)
+		p.setup_projectile(bird, left, right, ground, sky, speed)
 		p.bird_hit.connect(func(x, y, z): bird_hit.emit(x, y, z))
 		queued.append(p)
 	
