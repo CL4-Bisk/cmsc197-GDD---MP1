b73e866f7ede4483e88a73a733bb35246c734a45
G4l3S <gabs.sumergido@gmail.com>
2026-02-26 21:17:43 +0800

    misc: minor code fixes

--- revised/obstacles/common_spawner.gd
+++ revised/obstacles/common_spawner.gd
@@ -1,18 +1,25 @@
 extends Node
-class_name ProjectileSpawner
+class_name CommonSpawner
 
-signal bird_stunned
-signal bird_hurt
+signal bird_hit
 
 @export var projectiles : Array[PackedScene]
 @export var max_spawn_interval := 3.0
 
+@onready var right: Marker2D = $Right
+@onready var ground: Marker2D = $Ground
+@onready var left: Marker2D = $Left
+
 @onready var spawn_timer: Timer = $SpawnTimer
+var bird = null
 
 var bird_level := 0
 
 func _ready() -> void:
 	spawn_timer.timeout.connect(spawn_projectiles)
+
+func parse(birdie : Node2D) -> void:
+	bird = birdie
 
 func start() -> void:
 	spawn_timer.start()
@@ -22,12 +29,8 @@ func spawn_projectiles() -> void:
 	for i in range(randi_range(1, 2 + bird_level)):
 		var p = projectiles[
 			randi_range(0, projectiles.size()-1)].instantiate() as Projectile
-		
-		p.global_position.x = $SkyR.global_position.x
-		p.global_position.y = randf_range($SkyR.global_position.y, $GroundR.global_position.y)
-		
-		p.bird_hurt.connect(func(x): bird_hurt.emit(x))
-		p.bird_stun.connect(func(x): bird_stunned.emit(x))
+		p.setup_projectile(bird, left, right, ground)
+		p.bird_hit.connect(func(x, y, z): bird_hit.emit(x, y, z))
 		queued.append(p)
 	
 	spawn_timer.start(randf_range(0.05, max_spawn_interval))
