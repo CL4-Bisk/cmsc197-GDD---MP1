9e1e7961fe58cefaebe4aa6e1118b68428d5f9d9
G4l3S <gabs.sumergido@gmail.com>
2026-02-27 03:48:36 +0800

    refactor: optimized code for better readability, corrected sprite flipping logic based on face direction
    - corrected spawn location type

--- revised/obstacles/projectile.gd
+++ revised/obstacles/projectile.gd
@@ -7,7 +7,8 @@ signal bird_hit
 enum SpawnPositions {
 	LEFT_SIDE,
 	RIGHT_SIDE,
-	AERIAL
+	AERIAL,
+	GROUND
 }
 @export var delete_on_collide := false
 @export var chase_bird := false
@@ -23,7 +24,6 @@ var bird = null
 func _ready() -> void:
 	$Fire.play()
 	$Texture.speed_scale = randf_range(0.9, 1.1)
-	speed = randf_range(speed * 0.9, speed * 1.1)
 	
 	if chase_duration > 0:
 		var t = Timer.new()
@@ -34,40 +34,46 @@ func _ready() -> void:
 	if aim_to_bird and bird:
 		move_dir = global_position.direction_to(bird.global_position)
 		look_at(bird.global_position)
-	else:
-		if !chase_bird: 
-			$Texture.flip_h = (spawn_location == SpawnPositions.RIGHT_SIDE)
 
 func _process(delta: float) -> void:
 	if chase_bird and bird:
-		move_dir = global_position.direction_to(bird.global_position)
-		look_at(bird.global_position)
-	
+		if bird.is_alive: 
+			move_dir = global_position.direction_to(bird.global_position)
+			look_at(bird.global_position)
 	global_position += move_dir * speed * delta
 
-func setup_projectile(birdie : Node2D, left : Marker2D, right : Marker2D, ground : Marker2D) -> void:
+func setup_projectile(birdie : Node2D, l: float, r: float, g: float, s: float, spd: float) -> void:
 	bird = birdie
 	match spawn_location:
 		SpawnPositions.LEFT_SIDE, SpawnPositions.RIGHT_SIDE:
-			global_position.y = randf_range(left.global_position.y, ground.global_position.y)
+			global_position.y = randf_range(l, g)
 			if spawn_location == SpawnPositions.LEFT_SIDE:
-				global_position.x = left.global_position.x
+				global_position.x = l
 				move_dir = Vector2.RIGHT
 			else:
-				global_position.x = right.global_position.x
+				global_position.x = r
 				move_dir = Vector2.LEFT
 		SpawnPositions.AERIAL:
-			global_position.x = randf_range(left.global_position.x, right.global_position.x)
-			global_position.y = right.global_position.y
+			global_position.x = randf_range(l, r)
+			global_position.y = s
 			move_dir = Vector2.DOWN
+		SpawnPositions.GROUND:
+			global_position = Vector2(r, g)
+			move_dir = Vector2.LEFT
+			speed = spd
 
-func _hit(body: Node2D) -> void:
+	if !chase_bird: 
+		$Texture.flip_h = (move_dir == Vector2.LEFT)
+
+
+func _hit(_body: Node2D) -> void:
 	$Hit.play()
 	bird_hit.emit(false, damage_amount, stun_duration)
 	chase_bird = false
 	
 	if delete_on_collide:
 		$Texture.visible = false
+		$Hitbox.call_deferred("set_disabled", true)
 		await $Hit.finished
 		queue_free()
 
